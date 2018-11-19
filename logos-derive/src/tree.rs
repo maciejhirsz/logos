88492bd9c82151f8b8c391a7c7542f259c2e95c7
maciejhirsz <maciej.hirsz@gmail.com>
2018-11-19 17:33:42 +0100

    Going TDD route, this is insanity

--- logos-derive/src/tree.rs
+++ logos-derive/src/tree.rs
@@ -167,30 +167,43 @@ impl<'a> Fork<'a> {
     pub fn insert(&mut self, then: Node<'a>) {
         match then {
             Node::Branch(branch) => self.insert_branch(branch),
-            Node::Token(_) => {
-                self.collapse();
-                self.then = Some(then.boxed());
+            Node::Token(token) => {
+                if self.then.is_none() {
+                    assert!(
+                        self.kind == ForkKind::Plain,
+                        "Internal Error: Invalid fork construction: {:#?}", self
+                    );
+
+                    self.kind = ForkKind::Maybe;
+                    self.then = Some(Node::Token(token).boxed());
+                } else {
+                    // panic!("{} into {:#?}", token, self);
+
+                    assert!(
+                        self.kind != ForkKind::Plain,
+                        "Internal Error: Invalid fork construction: {:#?}", self
+                    );
+                }
+
+                // FIXME: look up through all tokens produced by `self.then`,
+                //        if they point at a token different from `token`,
+                //        panic with an error about conflicting definitions!
             },
-            Node::Fork(mut other)    => {
+            Node::Fork(mut other) => {
                 if self.kind == other.kind && self.arms == other.arms {
+                    // let a = self.clone();
+                    // let b = other.clone();
                     self.insert_then(other.then.take());
+
+                    // panic!("MERGE PRODUCT\n\nA: {:#?}\n\nB: {:#?}\n\n= {:#?}", a, b, self);
 
                     return;
                 }
 
-                if let Some(next) = other.unwind() {
-                    // FIXME: this if could be an assert that panics, but that
-                    //        would break the top fork when people do things like
-                    //        [0-9]* instead of [0-9]+ for ints
-                    //
-                    // Maybe throw a warning?
-                    if self.kind == ForkKind::Plain && self.then.is_none() {
-                        self.kind = ForkKind::Maybe;
-                        self.then = Some(next);
-                    }
-                }
+                other.unwind();
+                let token = other.collapse();
 
-                other.collapse();
+                self.insert_then(token);
 
                 for branch in other.arms.into_iter() {
                     self.insert_branch(branch);
@@ -201,10 +214,10 @@ impl<'a> Fork<'a> {
 
     pub fn insert_branch(&mut self, mut branch: Branch<'a>) {
         if branch.regex.len() == 0 {
-            if let Some(then) = branch.then {
-                return self.insert(*then);
-            }
+            return self.insert_then(branch.then);
         }
+
+        let mut byproduct = None;
 
         // Look for a branch that matches the same prefix
         for other in self.arms.iter_mut() {
@@ -221,33 +234,18 @@ impl<'a> Fork<'a> {
             }
 
             if let Some(prefix) = branch.regex.common_prefix(&other.regex) {
-                let regex = Regex::from(prefix);
+                let mut intersection = Branch::new(Regex::from(prefix));
 
-                let mut fork = Fork::default();
-                let mut new = Branch {
-                    regex,
-                    then: Some(fork.into()),
-                };
+                let mut a = branch.clone();
+                let mut b = other.clone();
 
-                if branch.regex.first() == new.regex.first() {
-                    let mut other = other.clone();
-                    let mut old = mem::replace(&mut branch, new);
+                a.regex.unshift();
+                b.regex.unshift();
 
-                    old.regex.unshift();
-                    other.regex.unshift();
+                intersection.insert_then(b.to_node().map(Box::new));
 
-                    branch.insert_then(old.to_node().map(Box::new));
-                    branch.insert_then(other.to_node().map(Box::new));
-                } else if other.regex.first() == new.regex.first() {
-                    let mut branch = branch.clone();
-                    let mut old = mem::replace(other, new);
-
-                    old.regex.unshift();
-                    branch.regex.unshift();
-
-                    other.insert_then(old.to_node().map(Box::new));
-                    other.insert_then(branch.to_node().map(Box::new));
-                }
+                // Cannot insert while we are still looping
+                byproduct = Some(intersection);
             }
         }
 
@@ -260,6 +258,10 @@ impl<'a> Fork<'a> {
                 self.arms.insert(index, branch.into());
             },
         }
+
+        if let Some(byproduct) = byproduct {
+            self.insert_branch(byproduct);
+        }
     }
 
     pub fn insert_then(&mut self, other: Option<Box<Node<'a>>>) {
@@ -270,50 +272,88 @@ impl<'a> Fork<'a> {
 
                     Some(then)
                 },
-                None => Some(other)
+                None => {
+                    assert!(
+                        self.kind == ForkKind::Plain,
+                        "Internal Error: Invalid fork construction: {:#?}", self
+                    );
+
+                    self.kind = ForkKind::Maybe;
+
+                    Some(other)
+                },
             };
         }
     }
 
-    /// Unwinds a Repeat fork, if successful returns the old `then`
-    /// variant
-    fn unwind(&mut self) -> Option<Box<Node<'a>>> {
+    /// Unwinds a Repeat fork into a Maybe fork
+    fn unwind(&mut self) {
+        // let before = self.clone();
+
         if self.kind != ForkKind::Repeat {
-            return None;
+            return;
         }
 
         let repeat = self.clone();
         let repeat = mem::replace(self, repeat);
 
-        let ret = self.then.take();
+        let then = self.then.take();
 
         self.then = Some(Node::from(repeat).boxed());
+        self.kind = ForkKind::Maybe;
 
-        ret
+        if let Some(then) = then {
+            self.insert(*then);
+        }
+
+        // panic!("Unwound {:#?}\n\nInto {:#?}", before, self);
     }
 
-    fn collapse(&mut self) {
-        let then = match self.then {
-            None => return,
-            Some(ref node) => &**node,
+    // Attempts to collapse a Maybe fork into a Plain fork.
+    // If `then` on this fork is a `Token`, then return it
+    fn collapse(&mut self) -> Option<Box<Node<'a>>> {
+        // let before = self.clone();
+
+        if self.kind != ForkKind::Maybe {
+            return None;
+        }
+
+        let then = match self.then.take() {
+            None => panic!("Invalid fork construction: {:#?}", self),
+            Some(node) => node,
         };
 
         for branch in self.arms.iter_mut() {
-            branch.append_at_end(then);
+            branch.append_at_end(&*then);
         }
+
+        if then.is_token() {
+            Some(then)
+        } else {
+            None
+        }
+
+        // panic!("Collapsed {:#?}\n\nInto {:#?}", before, self);
     }
 
     fn append_at_end(&mut self, then: &Node<'a>) {
-        match self.then {
-            Some(ref mut node) => node.append_at_end(then),
-            None => {
-                self.then = Some(then.clone().boxed());
-            },
-        }
+        // match self.then {
+        //     Some(ref mut node) => node.append_at_end(then),
+        //     None => {
+        //         self.then = Some(then.clone().boxed());
+        //     },
+        // }
 
         if self.kind == ForkKind::Plain {
             for branch in self.arms.iter_mut() {
                 branch.append_at_end(then)
+            }
+        } else {
+            match self.then {
+                Some(ref mut node) => node.append_at_end(then),
+                None => {
+                    self.then = Some(then.clone().boxed());
+                },
             }
         }
     }
@@ -335,20 +375,16 @@ impl<'a> Node<'a> {
         let fork = match self {
             Node::Fork(fork) => return fork,
             Node::Branch(branch) => {
-                let arm = Branch {
-                    regex: branch.regex.clone(),
-                    then: None,
-                };
-
+                // FIXME: a memswap of the branch could save an allocation here
                 Fork {
                     kind: ForkKind::Plain,
-                    arms: vec![arm],
-                    then: branch.then.clone(),
+                    arms: vec![branch.clone()],
+                    then: None,
                 }
             },
             Node::Token(_) => {
                 Fork {
-                    kind: ForkKind::Plain,
+                    kind: ForkKind::Maybe,
                     arms: vec![],
                     then: Some(self.clone().boxed()),
                 }
@@ -497,7 +533,104 @@ impl<'a> Node<'a> {
         }
     }
 
+    fn is_token(&self) -> bool {
+        match self {
+            Node::Token(_) => true,
+            _ => false,
+        }
+    }
+
     pub fn boxed(self) -> Box<Self> {
         Box::new(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use syn::Ident;
+
+    fn token(mock: &str) -> Ident {
+        use ::proc_macro2::Span;
+
+        Ident::new(mock, Span::call_site())
+    }
+
+    #[test]
+    fn branch_to_node() {
+        let regex = Regex::sequence("abc");
+        let branch = Branch::new(regex.clone());
+
+        assert_eq!(branch.to_node(), Some(Node::Branch(Branch {
+            regex: regex,
+            then: None
+        })));
+    }
+
+    #[test]
+    fn empty_branch_to_node() {
+        let branch = Branch::new(Regex::default());
+
+        assert_eq!(branch.to_node(), None);
+    }
+
+    #[test]
+    fn empty_branch_with_then_to_node() {
+        let token = token("mock");
+        let branch = Branch {
+            regex: Regex::default(),
+            then: Some(Node::Token(&token).boxed()),
+        };
+
+        assert_eq!(branch.to_node(), Some(Node::Token(&token)));
+    }
+
+    #[test]
+    fn insert_branch_into_branch() {
+        let token_a = token("ABC");
+        let token_b = token("DEF");
+
+        let branch_a = Branch {
+            regex: Regex::sequence("abc"),
+            then: Some(Node::Token(&token_a).boxed()),
+        };
+
+        let branch_b = Branch {
+            regex: Regex::sequence("def"),
+            then: Some(Node::Token(&token_b).boxed()),
+        };
+
+        let mut parent = branch_a.clone().to_node().unwrap();
+        let child = branch_b.clone().to_node().unwrap();
+
+        parent.insert(child);
+
+        assert_eq!(parent, Node::Fork(Fork {
+            kind: ForkKind::Plain,
+            arms: vec![branch_a, branch_b],
+            then: None,
+        }));
+    }
+
+    #[test]
+    fn insert_a_token_into_a_branch() {
+        let token_a = token("ABC");
+        let token_b = token("DEF");
+
+        let branch = Branch {
+            regex: Regex::sequence("abc"),
+            then: Some(Node::Token(&token_a).boxed()),
+        };
+
+        let mut parent = branch.clone().to_node().unwrap();
+        let child = Node::Token(&token_b);
+
+        parent.insert(child);
+
+        assert_eq!(parent, Node::Fork(Fork {
+            kind: ForkKind::Maybe,
+            arms: vec![branch],
+            then: Some(Node::Token(&token_b).boxed()),
+        }));
     }
 }
