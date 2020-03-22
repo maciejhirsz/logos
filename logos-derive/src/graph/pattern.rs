56692aab3e4bac88bb969a06ec2bc6b85721c2ee
Maciej Hirsz <hello@maciej.codes>
2020-03-22 15:28:03 +0100

    Range iterator

--- logos-derive/src/graph/pattern.rs
+++ logos-derive/src/graph/pattern.rs
@@ -6,11 +6,64 @@ macro_rules! pat {
 
 pub type Pattern = Vec<Range>;
 
-#[cfg_attr(test, derive(PartialEq))]
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub struct Range(pub u8, pub u8);
 
 impl From<u8> for Range {
     fn from(byte: u8) -> Range {
         Range(byte, byte)
+    }
+}
+
+impl Iterator for Range {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<u8> {
+        if self.0 < self.1 {
+            let res = self.0;
+            self.0 += 1;
+
+            Some(res)
+        } else if self.0 == self.1 {
+            let res = self.0;
+
+            // Necessary so that range 0xFF-0xFF doesn't loop forever
+            self.0 = 0xFF;
+            self.1 = 0x00;
+
+            Some(res)
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn range_iter_one() {
+        let byte = Range::from(b'!');
+        let collected = byte.into_iter().take(1000).collect::<Vec<_>>();
+
+        assert_eq!(b"!", &collected[..]);
+    }
+
+    #[test]
+    fn range_iter_few() {
+        let byte = Range(b'a', b'd');
+        let collected = byte.into_iter().take(1000).collect::<Vec<_>>();
+
+        assert_eq!(b"abcd", &collected[..]);
+    }
+
+    #[test]
+    fn range_iter_bunds() {
+        let byte = Range::from(0xFA..=0xFF);
+
+        let collected = byte.into_iter().take(1000).collect::<Vec<_>>();
+
+        assert_eq!(b"\xFA\xFB\xFC\xFD\xFE\xFF", &collected[..]);
     }
 }
