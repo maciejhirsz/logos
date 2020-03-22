45ffd3270ec3c73bf1b44b8c288f403c6ca6234a
Maciej Hirsz <hello@maciej.codes>
2020-03-22 09:35:42 +0100

    Disabled tests that are unwanted for now

---
+++ tests/tests/dev.rs
@@ -0,0 +1,34 @@
+use logos_derive::Logos;
+
+#[derive(Logos, PartialEq, Debug)]
+enum Token {
+    #[error]
+    Error,
+
+    #[end]
+    End,
+
+    #[token = "hell"]
+    Hell,
+
+    #[token = "HELLO!"]
+    #[token = "hello!"]
+    Hello,
+
+    #[regex = "[a-z]+"]
+    Ident,
+}
+
+mod simple {
+    // use super::*;
+    // use logos::Logos;
+    // use tests::assert_lex;
+
+    // #[test]
+    // fn empty() {
+    //     let lex = Token::lexer("");
+
+    //     assert_eq!(lex.token, Token::End);
+    //     assert_eq!(lex.range(), 0..0);
+    // }
+}
