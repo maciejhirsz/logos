f56f1762fce7cf667bf5e4a3c6ed180b608f79cc
matt rice <ratmice@gmail.com>
2020-04-05 05:55:41 -0700

    Test wrapping lexer modes in another iterator.

--- tests/tests/lexer_modes.rs
+++ tests/tests/lexer_modes.rs
@@ -1,3 +1,4 @@
+use logos::Lexer;
 use logos::Logos as _;
 use logos_derive::Logos;
 
@@ -53,4 +54,70 @@ fn main() {
     // We've exited the string, parser returns to outer lexer
     outer = inner.morph();
     assert_eq!(outer.next(), None);
+}
+
+enum Modes<'source> {
+    Outer(Lexer<'source, Outer>),
+    Inner(Lexer<'source, Inner>),
+}
+
+impl<'source> Modes<'source> {
+    fn new(s: &'source str) -> Self {
+        Self::Outer(Outer::lexer(s))
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Tokens {
+    InnerToken(Inner),
+    OuterToken(Outer),
+}
+
+struct ModeBridge<'source> {
+    mode: Modes<'source>,
+}
+
+// Clones as we switch between modes
+impl<'source> Iterator for ModeBridge<'source> {
+    type Item = Tokens;
+    fn next(&mut self) -> Option<Self::Item> {
+        use Tokens::*;
+        match &mut self.mode {
+            Modes::Inner(inner) => {
+                let result = inner.next();
+                if Some(Inner::EndString) == result {
+                    self.mode = Modes::Outer(inner.to_owned().morph());
+                }
+                result.map(InnerToken)
+            }
+            Modes::Outer(outer) => {
+                let result = outer.next();
+                if Some(Outer::StartString) == result {
+                    self.mode = Modes::Inner(outer.to_owned().morph());
+                }
+                result.map(OuterToken)
+            }
+        }
+    }
+}
+
+#[test]
+fn iterating_modes() {
+    use Inner::*;
+    use Tokens::*;
+    let s = r#""Hello W\u{00f4}rld\n""#;
+    let mut moded = ModeBridge {
+        mode: Modes::new(s),
+    };
+
+    let results: Vec<Tokens> = moded.collect();
+    let expect = vec![
+        OuterToken(Outer::StartString),
+        InnerToken(Text),
+        InnerToken(EscapedCodepoint),
+        InnerToken(Text),
+        InnerToken(EscapedNewline),
+        InnerToken(EndString),
+    ];
+    assert_eq!(results, expect);
 }
