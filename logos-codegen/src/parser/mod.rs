51c1f8c1bca990758e05c5600becc6d9c10bd6e4
Andrew Hickman <andrew.hickman1@sky.com>
2022-06-14 09:59:04 +0100

    Add a CLI which can be used for codegeneration (#248)
    * Add logos-codegen CLI
    
    * Strip logos attributes
    
    * Add some tests
    
    * cargo fmt
    
    * Normalize newline characters in logos-cli
    
    * Fix tests on windows
    
    * Check rustfmt exit code
    
    * Include rustfmt tool for tests

--- logos-codegen/src/parser/mod.rs
+++ logos-codegen/src/parser/mod.rs
@@ -7,6 +7,7 @@ use syn::{Attribute, GenericParam, Lit, Type};
 use crate::error::Errors;
 use crate::leaf::{Callback, InlineCallback};
 use crate::util::{expect_punct, MaybeVoid};
+use crate::LOGOS_ATTR;
 
 mod definition;
 mod ignore_flags;
@@ -71,7 +72,7 @@ impl Parser {
     /// Try to parse the main `#[logos(...)]`, does nothing if
     /// the attribute's name isn't `logos`.
     pub fn try_parse_logos(&mut self, attr: &mut Attribute) {
-        if !attr.path.is_ident("logos") {
+        if !attr.path.is_ident(LOGOS_ATTR) {
             return;
         }
 
