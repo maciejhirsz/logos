c99181782e17e1de602bab0c55d063b2e4bac310
Jérome Eertmans <jeertmans@icloud.com>
2024-02-16 11:04:42 +0100

    Revert "chore(lib): rewrite using ClassUnicode methods"
    This reverts commit 80bd23fd112b7ab0efbb02ca278cb75d02dcd195.

--- logos-codegen/src/graph/regex.rs
+++ logos-codegen/src/graph/regex.rs
@@ -160,15 +160,25 @@ impl<Leaf: Disambiguate + Debug> Graph<Leaf> {
     }
 }
 
-#[inline]
 fn is_ascii(class: &ClassUnicode) -> bool {
-    class.is_ascii()
+    class.iter().all(|range| {
+        let start = range.start() as u32;
+        let end = range.end() as u32;
+
+        start < 128 && end < 128
+    })
 }
 
-#[inline]
 fn is_one_ascii(class: &ClassUnicode) -> bool {
-    let ranges = class.ranges();
-    ranges.len() == 1 && ranges[0].end() < '\x7F'
+    if class.ranges().len() != 1 {
+        return false;
+    }
+
+    let range = &class.ranges()[0];
+    let start = range.start() as u32;
+    let end = range.end() as u32;
+
+    start < 128 && end < 128
 }
 
 #[cfg(test)]
