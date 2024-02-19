7ce05269ccd20f65614c3ed5173ad674eaa426ed
afreeland <afreeland@gmail.com>
2024-02-19 11:58:29 -0500

    Add debug support and documentation for internal graph output

--- logos-codegen/src/generator/mod.rs
+++ logos-codegen/src/generator/mod.rs
@@ -53,6 +53,9 @@ impl<'a> Generator<'a> {
         let rendered = Self::fast_loop_macro();
         let meta = Meta::analyze(root, graph);
 
+        #[cfg(feature = "debug")]
+        dbg!(graph);
+
         Generator {
             name,
             this,
