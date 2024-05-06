d2e0ebfab2da7e60122e504e6bd4978b5f660b23
RustyYato <rustyyato@gmail.com>
2024-05-06 12:06:10 -0700

    Revert "add codegen tests for LUT"
    This reverts commit 0dfcf0080c52c79ce5c5d1e4ce3edbf021390c28.

--- logos-cli/tests/tests.rs
+++ logos-cli/tests/tests.rs
@@ -4,80 +4,47 @@ use assert_cmd::Command;
 use assert_fs::{assert::PathAssert, fixture::FileWriteStr, NamedTempFile};
 use predicates::prelude::*;
 
-struct Fixture {
-    input: &'static str,
-    output: &'static str,
-    fmt_output: &'static str,
-}
-
-const FIXTURES: &[Fixture] = &[
-    Fixture {
-        input: concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data/simple/input.rs"),
-        output: concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data/simple/output.rs"),
-        fmt_output: concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/tests/data/simple/fmt_output.rs"
-        ),
-    },
-    Fixture {
-        input: concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/tests/data/no_error_lut/input.rs"
-        ),
-        output: concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/tests/data/no_error_lut/output.rs"
-        ),
-        fmt_output: concat!(
-            env!("CARGO_MANIFEST_DIR"),
-            "/tests/data/no_error_lut/fmt_output.rs"
-        ),
-    },
-];
+const INPUT_FILE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data/input.rs");
+const OUTPUT_FILE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data/output.rs");
+const FMT_OUTPUT_FILE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data/fmt_output.rs");
 
 #[test]
 fn test_codegen() {
-    for fixture in FIXTURES.iter() {
-        let tempfile = NamedTempFile::new("output.gen.rs").unwrap();
+    let tempfile = NamedTempFile::new("output.gen.rs").unwrap();
 
-        let mut cmd = Command::cargo_bin("logos-cli").unwrap();
-        cmd.arg(fixture.input)
-            .arg("--output")
-            .arg(tempfile.path())
-            .assert()
-            .success();
+    let mut cmd = Command::cargo_bin("logos-cli").unwrap();
+    cmd.arg(INPUT_FILE)
+        .arg("--output")
+        .arg(tempfile.path())
+        .assert()
+        .success();
 
-        tempfile.assert(normalize_newlines(fixture.output));
-    }
+    tempfile.assert(normalize_newlines(OUTPUT_FILE));
 }
 
 #[test]
 fn test_codegen_check() {
-    for fixture in FIXTURES.iter() {
-        Command::cargo_bin("logos-cli")
-            .unwrap()
-            .arg(fixture.input)
-            .arg("--check")
-            .arg("--output")
-            .arg(fixture.output)
-            .assert()
-            .success();
-    }
+    Command::cargo_bin("logos-cli")
+        .unwrap()
+        .arg(INPUT_FILE)
+        .arg("--check")
+        .arg("--output")
+        .arg(OUTPUT_FILE)
+        .assert()
+        .success();
 }
 
 #[test]
 fn test_codegen_check_format() {
-    for fixture in FIXTURES.iter() {
-        Command::cargo_bin("logos-cli")
-            .unwrap()
-            .arg(fixture.input)
-            .arg("--format")
-            .arg("--check")
-            .arg("--output")
-            .arg(fixture.fmt_output)
-            .assert()
-            .success();
-    }
+    Command::cargo_bin("logos-cli")
+        .unwrap()
+        .arg(INPUT_FILE)
+        .arg("--format")
+        .arg("--check")
+        .arg("--output")
+        .arg(FMT_OUTPUT_FILE)
+        .assert()
+        .success();
 }
 
 #[test]
@@ -88,7 +55,7 @@ fn test_codegen_fail_check() {
 
     Command::cargo_bin("logos-cli")
         .unwrap()
-        .arg(FIXTURES[0].input)
+        .arg(INPUT_FILE)
         .arg("--check")
         .arg("--output")
         .arg(tempfile.path())
@@ -98,19 +65,17 @@ fn test_codegen_fail_check() {
 
 #[test]
 fn test_codegen_format() {
-    for fixture in FIXTURES {
-        let tempfile = NamedTempFile::new("output.gen.rs").unwrap();
+    let tempfile = NamedTempFile::new("output.gen.rs").unwrap();
 
-        let mut cmd = Command::cargo_bin("logos-cli").unwrap();
-        cmd.arg(fixture.input)
-            .arg("--format")
-            .arg("--output")
-            .arg(tempfile.path())
-            .assert()
-            .success();
+    let mut cmd = Command::cargo_bin("logos-cli").unwrap();
+    cmd.arg(INPUT_FILE)
+        .arg("--format")
+        .arg("--output")
+        .arg(tempfile.path())
+        .assert()
+        .success();
 
-        tempfile.assert(normalize_newlines(fixture.fmt_output));
-    }
+    tempfile.assert(normalize_newlines(FMT_OUTPUT_FILE));
 }
 
 fn normalize_newlines(s: impl AsRef<Path>) -> impl Predicate<str> {
