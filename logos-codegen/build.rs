fc71df4372c02bc06c6ad0469fa3a510613a8dba
Bilal Mahmoud <bmahmoud@mpi-cbg.de>
2024-11-13 10:04:15 +0100

    chore: add documentation about cfg flag

--- logos-codegen/build.rs
+++ logos-codegen/build.rs
@@ -10,6 +10,10 @@ fn main() {
         rustc_version.patch,
     );
 
+    // Add cfg flag for Rust >= 1.82
+    // Required for precise capturing in edition 2024
+    // Due to changes in lifetime and type capture behavior for impl trait
+    // see: https://github.com/maciejhirsz/logos/issues/434, https://github.com/rust-lang/rfcs/pull/3498
     println!("cargo:rustc-check-cfg=cfg(rust_1_82)");
     if trimmed_rustc_version >= Version::new(1, 82, 0) {
         println!("cargo:rustc-cfg=rust_1_82");
