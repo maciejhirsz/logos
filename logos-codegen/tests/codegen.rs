dd2032e100c28855889dced6bf16e953408d887f
mysteriouslyseeing <61419567+mysteriouslyseeing@users.noreply.github.com>
2025-03-13 23:41:45 +1100

    converted codegen tests to insta

--- logos-codegen/tests/codegen.rs
+++ logos-codegen/tests/codegen.rs
@@ -1,8 +1,6 @@
-use std::{
-    error::Error,
-    io::{self, Read, Seek},
-    path::PathBuf,
-};
+use std::{error::Error, path::PathBuf};
+
+use insta::assert_yaml_snapshot;
 
 #[rstest::rstest]
 #[case("simple")]
@@ -17,37 +15,17 @@ pub fn test_codegen(#[case] fixture: &str) -> Result<(), Box<dyn Error>> {
     fixture_dir.push(fixture);
 
     let input = fixture_dir.join("input.rs");
-    #[cfg(rust_1_82)]
-    fixture_dir.push("output.rs");
-    #[cfg(not(rust_1_82))]
-    fixture_dir.push("output-pre_1_82.rs");
-    let output_file_path = fixture_dir;
-
     let input = std::fs::read_to_string(input)?;
 
-    let mut output = String::new();
-
-    // we want to create the output file if it doesn't exist
-    let mut output_file = std::fs::OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(&output_file_path)?;
-
-    output_file.read_to_string(&mut output)?;
-
     let generated = logos_codegen::generate(input.parse()?);
-    let generated = generated.to_string();
+    let generated: syn::ItemImpl = syn::parse2(generated)?;
+    let generated = syn_serde::Syn::to_adapter(&generated);
 
-    if std::env::var("BLESS_CODEGEN").is_ok_and(|value| value == "1") {
-        use std::io::{Seek as _, Write as _};
-        output_file.set_len(0)?;
-        output_file.seek(io::SeekFrom::Start(0))?;
-        output_file.write_all(generated.as_bytes())?;
-        return Ok(());
+    if cfg!(rust_1_82) {
+        assert_yaml_snapshot!(format!("{fixture}-1_82"), generated);
+    } else {
+        assert_yaml_snapshot!(format!("{fixture}-pre_1_82"), generated);
     }
-
-    assert_eq!(generated, output, "Codegen test failed: `{fixture}`, run tests again with env var `BLESS_CODEGEN=1` to bless these changes");
 
     Ok(())
 }
