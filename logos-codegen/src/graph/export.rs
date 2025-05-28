fedaf6710ef222e5224a79a5ae7d6b58278c4409
ShadowCat650 <64231061+Shadowcat650@users.noreply.github.com>
2025-05-28 09:20:56 -0700

    reduce code duplication

--- logos-codegen/src/graph/export.rs
+++ logos-codegen/src/graph/export.rs
@@ -3,250 +3,140 @@ use crate::graph::{Fork, Graph, Node, NodeId, Range, Rope};
 use std::collections::HashMap;
 use std::fmt::{Display, Write};
 
-struct DotNodeIds {
+/// Helps assign short strings to node ids while avoiding name collisions.
+struct NodeIdStrings {
     next: u32,
     mappings: HashMap<usize, String>,
 }
 
-impl DotNodeIds {
-    fn next(&mut self) -> String {
+impl NodeIdStrings {
+    /// Get a unique string for a node.
+    fn get_unique(&mut self) -> String {
         let next = self.next;
         self.next += 1;
         format!("n{:x}", next)
     }
 
+    /// Get the string assigned to a node from its index.
     fn idx(&mut self, id: usize) -> &str {
         if !self.mappings.contains_key(&id) {
-            let next = self.next();
+            let next = self.get_unique();
             self.mappings.insert(id, next);
         }
         self.mappings.get(&id).unwrap().as_str()
     }
 
+    /// Get the string assigned to a node from its id.
     fn node(&mut self, node: NodeId) -> &str {
         self.idx(node.get())
     }
 }
 
-impl<Leaf: Display> Graph<Leaf> {
-    /// Writes the `Graph` to a dot file.
-    pub fn get_dot(&self) -> Result<String, std::fmt::Error> {
-        let mut s = String::new();
+enum NodeColor {
+    Red,
+    Blue,
+    Green,
+    Orange,
+}
 
-        let entries = self
-            .nodes()
-            .iter()
-            .enumerate()
-            .filter_map(|(i, n)| n.as_ref().map(|n| (i, n)));
-
-        let mut ids = DotNodeIds {
-            next: 0,
-            mappings: HashMap::new(),
-        };
-
-        write!(s, "digraph Lexer{{")?;
-        write!(s, "node[shape=box];")?;
-        write!(s, "splines=ortho;")?;
-        for (id, node) in entries {
-            match node {
-                Node::Fork(fork) => fork.write_dot(&mut s, &mut ids, id)?,
-                Node::Rope(rope) => rope.write_dot(&mut s, &mut ids, id)?,
-                Node::Leaf(leaf) => {
-                    write!(s, "{}[label=\"{}\",color=green];", ids.idx(id), leaf)?;
-                }
-            }
+impl NodeColor {
+    fn fmt_dot(&self) -> &'static str {
+        match self {
+            Self::Red => "red",
+            Self::Blue => "blue",
+            Self::Green => "green",
+            Self::Orange => "orange",
         }
-        write!(s, "}}")?;
-        Ok(s)
     }
 
-    /// Writes the `Graph` to a mermaid file.
-    pub fn get_mmd(&self) -> Result<String, std::fmt::Error> {
-        let mut s = String::new();
-
-        let entries = self
-            .nodes()
-            .iter()
-            .enumerate()
-            .filter_map(|(i, n)| n.as_ref().map(|n| (i, n)));
-
-        let mut ids = DotNodeIds {
-            next: 0,
-            mappings: HashMap::new(),
-        };
-
-        writeln!(s, "---")?;
-        writeln!(s, "title: Lexer")?;
-        writeln!(s, "---")?;
-        writeln!(s, "flowchart TB")?;
-        for (id, node) in entries {
-            match node {
-                Node::Fork(fork) => fork.write_mmd(&mut s, &mut ids, id)?,
-                Node::Rope(rope) => rope.write_mmd(&mut s, &mut ids, id)?,
-                Node::Leaf(leaf) => {
-                    writeln!(
-                        s,
-                        "{}[\"{}\"]",
-                        ids.idx(id),
-                        leaf.to_string().replace('<', "&lt").replace('>', "&gt")
-                    )?;
-                    writeln!(s, "style {} stroke:#00C853", ids.idx(id))?;
-                }
-            }
+    fn fmt_mmd(&self) -> &'static str {
+        match self {
+            Self::Red => "#D50000",
+            Self::Blue => "#2962FF",
+            Self::Green => "#00C853",
+            Self::Orange => "#FF6D00",
         }
-        Ok(s)
     }
 }
 
-impl Fork {
-    fn write_dot(
-        &self,
-        s: &mut String,
-        ids: &mut DotNodeIds,
-        id: usize,
-    ) -> Result<(), std::fmt::Error> {
-        let id = ids.idx(id).to_string();
-        write!(s, "{}[label=\"Fork\",color=dodgerblue];", id)?;
-        for (range, node) in self.branches() {
-            let link_id = ids.next();
-            write!(
-                s,
-                "{}[label=\"{}\",color=orange];",
-                link_id,
-                range.fmt_dot()
-            )?;
-            write!(s, "{}->{};", id, link_id)?;
-            write!(s, "{}->{};", link_id, ids.node(node))?;
-        }
-        if let Some(miss) = self.miss {
-            write!(s, "{}->{};", id, ids.node(miss))?;
-        }
-        Ok(())
-    }
+trait ExportFormat {
+    fn write_header(s: &mut String) -> std::fmt::Result;
 
-    fn write_mmd(
-        &self,
-        s: &mut String,
-        ids: &mut DotNodeIds,
-        id: usize,
-    ) -> Result<(), std::fmt::Error> {
-        let id = ids.idx(id).to_string();
-        writeln!(s, "{}[\"Fork\"]", id)?;
-        writeln!(s, "style {} stroke:#2962FF", id)?;
-        for (range, node) in self.branches() {
-            let link_id = ids.next();
-            writeln!(s, "{}[\"{}\"]", link_id, range.fmt_mmd())?;
-            writeln!(s, "style {} stroke:#FF6D00", link_id)?;
-            writeln!(s, "{}-->{};", id, link_id)?;
-            writeln!(s, "{}-->{};", link_id, ids.node(node))?;
-        }
-        if let Some(miss) = self.miss {
-            write!(s, "{}-->{};", id, ids.node(miss))?;
-        }
-        Ok(())
-    }
-}
+    fn write_footer(s: &mut String) -> std::fmt::Result;
 
-impl Rope {
-    fn write_dot(
-        &self,
-        s: &mut String,
-        ids: &mut DotNodeIds,
-        id: usize,
-    ) -> Result<(), std::fmt::Error> {
-        let id = ids.idx(id).to_string();
-        write!(s, "{}[label=\"Rope\",color=blueviolet];", id)?;
+    fn write_node(s: &mut String, id: &str, label: &str, color: NodeColor) -> std::fmt::Result;
 
-        let mut previous = id.clone();
-        for range in self.pattern.iter() {
-            let link_id = ids.next();
-            write!(
-                s,
-                "{}[label=\"{}\",color=orange];",
-                link_id,
-                range.fmt_dot()
-            )?;
-            write!(s, "{}->{};", previous, link_id)?;
-            previous = link_id;
-        }
-        write!(s, "{}->{};", previous, ids.node(self.then))?;
+    fn write_link(s: &mut String, from: &str, to: &str) -> std::fmt::Result;
 
-        match self.miss {
-            Miss::First(node) => {
-                let link_id = ids.next();
-                write!(
-                    s,
-                    "{}[label=\"NOT {}\",color=red];",
-                    link_id,
-                    self.pattern.first().unwrap().fmt_dot()
-                )?;
-                write!(s, "{}->{};", id, link_id)?;
-                write!(s, "{}->{};", link_id, ids.node(node))
-            }
-            Miss::Any(node) => {
-                write!(s, "{}->{};", id, ids.node(node))
-            }
-            Miss::None => Ok(()),
-        }
-    }
-
-    fn write_mmd(
-        &self,
-        s: &mut String,
-        ids: &mut DotNodeIds,
-        id: usize,
-    ) -> Result<(), std::fmt::Error> {
-        let id = ids.idx(id).to_string();
-        writeln!(s, "{}[\"Rope\"]", id)?;
-        writeln!(s, "style {} stroke:#2962FF", id)?;
-
-        let mut previous = id.clone();
-        for range in self.pattern.iter() {
-            let link_id = ids.next();
-            writeln!(s, "{}[\"{}\"]", link_id, range.fmt_mmd())?;
-            writeln!(s, "style {} stroke:#FF6D00", link_id)?;
-            writeln!(s, "{}-->{};", previous, link_id)?;
-            previous = link_id;
-        }
-        writeln!(s, "{}-->{};", previous, ids.node(self.then))?;
-
-        match self.miss {
-            Miss::First(node) => {
-                let link_id = ids.next();
-                writeln!(
-                    s,
-                    "{}[\"NOT {}\"]",
-                    link_id,
-                    self.pattern.first().unwrap().fmt_mmd()
-                )?;
-                writeln!(s, "style {} stroke:#D50000", link_id)?;
-                writeln!(s, "{}-->{};", id, link_id)?;
-                writeln!(s, "{}-->{};", link_id, ids.node(node))
-            }
-            Miss::Any(node) => {
-                writeln!(s, "{}-->{};", id, ids.node(node))
-            }
-            Miss::None => Ok(()),
-        }
-    }
+    fn fmt_range(r: &Range) -> String;
 }
 
 impl Range {
-    fn fmt_dot(&self) -> String {
-        if self.is_byte() && (0x20..0x7F).contains(&self.start) {
-            let escaped = (self.start as char)
-                .escape_default()
-                .flat_map(|c| c.escape_default());
-            format!("'{}'", escaped.collect::<String>())
+    fn fmt_with_escape<I: Iterator<Item = char>>(&self, escape: impl Fn(char) -> I) -> String {
+        let fmt_byte = |b: u8| -> String {
+            if (0x20..0x7F).contains(&b) {
+                let escaped = (b as char).escape_default().flat_map(|c| escape(c));
+                format!("'{}'", escaped.collect::<String>())
+            } else {
+                format!("{:02X}", b)
+            }
+        };
+
+        if let Some(b) = self.as_byte() {
+            fmt_byte(b)
         } else {
-            self.to_string()
-                .escape_default()
-                .flat_map(|c| c.escape_default())
-                .collect()
+            format!("{}..={}", fmt_byte(self.start), fmt_byte(self.end))
         }
     }
+}
 
-    fn fmt_mmd(&self) -> String {
+struct DOT;
+
+impl ExportFormat for DOT {
+    fn write_header(s: &mut String) -> std::fmt::Result {
+        write!(s, "digraph {{")?;
+        write!(s, "node[shape=box];")?;
+        write!(s, "splines=ortho;")
+    }
+
+    fn write_footer(s: &mut String) -> std::fmt::Result {
+        write!(s, "}}")
+    }
+
+    fn write_node(s: &mut String, id: &str, label: &str, color: NodeColor) -> std::fmt::Result {
+        write!(s, "{}[label=\"{}\",color={}];", id, label, color.fmt_dot())
+    }
+
+    fn write_link(s: &mut String, from: &str, to: &str) -> std::fmt::Result {
+        write!(s, "{}->{};", from, to)
+    }
+
+    fn fmt_range(r: &Range) -> String {
+        r.fmt_with_escape(|c| c.escape_default())
+    }
+}
+
+struct MMD;
+
+impl ExportFormat for MMD {
+    fn write_header(s: &mut String) -> std::fmt::Result {
+        writeln!(s, "flowchart TB")
+    }
+
+    fn write_footer(_s: &mut String) -> std::fmt::Result {
+        Ok(())
+    }
+
+    fn write_node(s: &mut String, id: &str, label: &str, color: NodeColor) -> std::fmt::Result {
+        writeln!(s, "{}[\"{}\"]", id, label)?;
+        writeln!(s, "style {} stroke:{}", id, color.fmt_mmd())
+    }
+
+    fn write_link(s: &mut String, from: &str, to: &str) -> std::fmt::Result {
+        writeln!(s, "{}-->{}", from, to)
+    }
+
+    fn fmt_range(r: &Range) -> String {
         fn escape_mmd(c: char) -> impl Iterator<Item = char> {
             enum Iter {
                 Char(Option<char>),
@@ -272,16 +162,114 @@ impl Range {
                 _ => Iter::Char(Some(c)),
             }
         }
-        if self.is_byte() && (0x20..0x7F).contains(&self.start) {
-            let escaped = (self.start as char)
-                .escape_default()
-                .flat_map(|c| escape_mmd(c));
-            format!("'{}'", escaped.collect::<String>())
-        } else {
-            self.to_string()
-                .escape_default()
-                .flat_map(|c| escape_mmd(c))
-                .collect()
+        r.fmt_with_escape(escape_mmd)
+    }
+}
+
+impl<Leaf: Display> Graph<Leaf> {
+    /// Writes the `Graph` to a dot file.
+    pub fn get_dot(&self) -> Result<String, std::fmt::Error> {
+        self.export_graph::<DOT>()
+    }
+
+    /// Writes the `Graph` to a mermaid file.
+    pub fn get_mmd(&self) -> Result<String, std::fmt::Error> {
+        self.export_graph::<MMD>()
+    }
+
+    fn export_graph<Fmt: ExportFormat>(&self) -> Result<String, std::fmt::Error> {
+        let mut s = String::new();
+
+        let entries = self
+            .nodes()
+            .iter()
+            .enumerate()
+            .filter_map(|(i, n)| n.as_ref().map(|n| (i, n)));
+
+        let mut ids = NodeIdStrings {
+            next: 0,
+            mappings: HashMap::new(),
+        };
+
+        Fmt::write_header(&mut s)?;
+        for (id, node) in entries {
+            match node {
+                Node::Fork(fork) => fork.write_graph::<Fmt>(&mut s, &mut ids, id)?,
+                Node::Rope(rope) => rope.write_graph::<Fmt>(&mut s, &mut ids, id)?,
+                Node::Leaf(leaf) => {
+                    Fmt::write_node(&mut s, ids.idx(id), &leaf.to_string(), NodeColor::Green)?;
+                }
+            }
+        }
+        Fmt::write_footer(&mut s)?;
+
+        Ok(s)
+    }
+}
+
+impl Fork {
+    fn write_graph<Fmt: ExportFormat>(
+        &self,
+        s: &mut String,
+        ids: &mut NodeIdStrings,
+        id: usize,
+    ) -> Result<(), std::fmt::Error> {
+        let id = ids.idx(id).to_string();
+        Fmt::write_node(s, &id, "Fork", NodeColor::Blue)?;
+        for (range, node) in self.branches() {
+            let link_id = ids.get_unique();
+            Fmt::write_node(s, &link_id, &Fmt::fmt_range(&range), NodeColor::Orange)?;
+            Fmt::write_link(s, &id, &link_id)?;
+            Fmt::write_link(s, &link_id, ids.node(node))?;
+        }
+        if let Some(miss) = self.miss {
+            Fmt::write_link(s, &id, ids.node(miss));
+        }
+        Ok(())
+    }
+}
+
+impl Rope {
+    fn write_graph<Fmt: ExportFormat>(
+        &self,
+        s: &mut String,
+        ids: &mut NodeIdStrings,
+        id: usize,
+    ) -> Result<(), std::fmt::Error> {
+        let id = ids.idx(id).to_owned();
+        Fmt::write_node(s, &id, "Rope", NodeColor::Blue)?;
+
+        let mut previous = id.clone();
+        for range in self.pattern.iter() {
+            let link_id = ids.get_unique();
+            Fmt::write_node(s, &link_id, &Fmt::fmt_range(&range), NodeColor::Orange)?;
+            Fmt::write_link(s, &previous, &link_id)?;
+            previous = link_id;
+        }
+        Fmt::write_link(s, &previous, ids.node(self.then))?;
+
+        match self.miss {
+            Miss::First(node) => {
+                let link_id = ids.get_unique();
+                Fmt::write_node(
+                    s,
+                    &link_id,
+                    &format!("NOT {}", Fmt::fmt_range(&self.pattern.first().unwrap())),
+                    NodeColor::Red,
+                )?;
+                Fmt::write_link(s, &id, &link_id)?;
+                Fmt::write_link(s, &link_id, ids.node(node))
+            }
+            Miss::Any(node) => Fmt::write_link(s, &id, ids.node(node)),
+            Miss::None => Ok(()),
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn t() {}
 }
